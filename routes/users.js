1c44e8bca55bc91f85b9581806a6020f37079ce6
Your name01~ <Your email[201~>
2023-07-28 15:41:46 +0200

    ajout routes users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/users')
+const User = require('../models/users');
+const uid2 = require('uid2');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', (req, res) => {
+  if(!checkBody(req.body, ["firstname", "lastname", "email", "password", "token", "connectionMode", "role"])){
+    res.json({result: false, error: "Missing or empty fields"});
+}
+const { firstname, lastname, email, password, connectionMode, role } = req.body
+
+  const newUser = new User({
+    firstname,
+    lastname,
+    email,
+    password,
+    token: uid2,
+    connectionMode,
+    role
+   });
+
+   newUser.save()
+   .then(data => {
+    console.log(data);
+    res.json({result: true, })
+   });
 });
+
+
+router.get('/', function(req, res) {
+  User.find({})
+  .then(data=> {
+     res.json({result: true, userList : data})
+  })
+});
+
+router.delete("/:id", (req, res) =>{
+  if(!checkBody(req.body, ["id"])){
+      res.json({ result: false, error: "Missing or empty fields"});
+      return;
+  }
+  const { id } = req.body;
+  User.deleteOne({ id: { $regex: new RegExp(id, "i")}})
+  .then(deletedDoc => {
+      if(deletedDoc.deletedCount > 0){
+          res.json({ result: true})
+      } else {
+          res.json({result: false, error: " not found"})
+      }
+  })
+})
 
 module.exports = router;
