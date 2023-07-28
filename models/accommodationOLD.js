4be20447fcc85ffcfe8a9504a225967b5d0c22d9
Benjamin Saragaglia <saragaglia.benjamin@gmail.com>
2023-07-28 11:36:49 +0200

    essai avec une fausse supression de fichier

---
+++ models/accommodationOLD.js
@@ -0,0 +1,16 @@
+//ANCIENNE VERSION DE WILLIAM
+const mongoose = require('mongoose');
+require('./connection');
+
+const accommodationSchema = mongoose.Schema({
+ name: String,
+ picture: String,
+ address: String,
+ description: String,
+ price: Number,
+ distribution: String,
+});
+
+const Accommodation = mongoose.model('accommodation', accommodationSchema);
+
+module.exports = Accommodation;
