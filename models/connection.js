7b7be5f100104b01caee1c32dcbf19d8d4ab3ca3
Alune62 <seydina.alioune9319@gmail.com>
2023-08-03 11:02:19 +0200

    back

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,9 @@
 
 
 const mongoose = require('mongoose');
+CONNECTION_STRING = "mongodb+srv://Alioune:E7hNRvzjkeMk7lrw@myfirst.0l30bb6.mongodb.net/StayNomad"
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
