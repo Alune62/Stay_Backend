6e90df989f66ec2cc95da6bfcb7c1cceb6be2523
Alune62 <seydina.alioune9319@gmail.com>
2023-08-03 10:45:38 +0200

    back

--- models/messages.js
+++ models/messages.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
 Text: String,
-username: { type: mongoose.Schema.Types.name, ref: 'username' },
+username: { type: mongoose.Schema.Types.ObjectId, ref: 'username' },
 createdAt: Date,
-accommodation: { type: mongoose.Schema.Types.name, ref: 'accomodation' }
+accommodation: { type: mongoose.Schema.Types.ObjectId, ref: 'accomodation' }
 });
 
 const Message = mongoose.model('messages', messageSchema);
